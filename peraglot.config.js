8d69312cadb13d938f1562c2eea114d029aad25e
James Roland <james.a.roland@gmail.com>
2024-05-17 08:45:25 -0700

    Swapping from json to js config file

---
+++ peraglot.config.js
@@ -0,0 +1,17 @@
+/** @type {import('peraglot-tools').config.PeraglotConfig} */
+const peraglotConfig = {
+  locales: {
+    en_US: {
+      isSource: true,
+      path: "public/lang/en.json",
+    },
+    ja_JP: {
+      path: "public/lang/ja.json",
+    },
+    zh_CN: {
+      path: "public/lang/zh.json",
+    },
+  },
+};
+
+module.exports = peraglotConfig;
