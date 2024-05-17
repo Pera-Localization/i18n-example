e7da7c0008e5ddac29053823c168a3c018ccd9df
James Roland <james.a.roland@gmail.com>
2024-05-17 08:10:23 -0700

    Basic e2e changes for peraglot

---
+++ peraglot.config.js
@@ -0,0 +1,24 @@
+// This file is NOT read by peraglot. It is only used to demonstrate how to typecheck the config object locally
+// You can copy the value of the variable, then paste it into your peraglot.config.json file for use.
+// Currently only peraglot.config.json is supported by the plugin
+
+/** @type {import('peraglot-tools').config.PeraglotConfigInput} */
+const peraglotConfig = {
+  source: {
+    locale: "en_US",
+    path: "public/lang/en.json",
+  },
+  targets: {
+    ja_JP: {
+      path: "public/lang/ja.json",
+    },
+    zh_CN: {
+      path: "public/lang/zh.json",
+    },
+  },
+  detectionBranch: "main",
+  translationBranch: "main",
+  publishBranch: "main",
+};
+
+module.exports = peraglotConfig;
